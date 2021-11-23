079947fb71ed18fea25a77d5b46a272ea0e1919a
Prim <demassia@naver.com>
2021-11-23 11:25:58 +0900

    intent화면전환

--- app/src/main/java/com/example/practice2/MainActivity.java
+++ app/src/main/java/com/example/practice2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.practice2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,21 +10,25 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_id;
-    Button btn_test;
+    private EditText et_test;
+    private Button btn_move;
+    private String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_id = findViewById(R.id.et_id);
-        btn_test = findViewById(R.id.btn_test);
+        et_test = findViewById(R.id.et_test);
 
-        btn_test.setOnClickListener(new View.OnClickListener() {
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                et_id.setText("123213");
+            public void onClick(View v) {
+                str = et_test.getText().toString();
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str",str);
+                startActivity(intent); //액티비티 이동
             }
         });
 
